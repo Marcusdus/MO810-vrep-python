1c0de597fbf46bcb4dbfe57deca00b294bd064dd
luwood <lu.madeira2@gmail.com>
2017-06-15 19:42:36 -0300

    Adding get distance method.

--- andabb/unittests/testSimulator.py
+++ andabb/unittests/testSimulator.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 from andabb.Simulator import Simulator
 
 LEFT_ENCODER = "Pioneer_p3dx_leftWheel"
@@ -46,6 +47,16 @@ class SimulatorTest(unittest.TestCase):
         handle = self.sim.getHandle("Pioneer_p3dx")
         pos = self.sim.getObjectPosition(handle)
         print("Pos {}".format(pos))
+
+    def testGetDistance(self):
+        notZero = 0
+        for i in range(0, 10):
+            dist = self.sim.getDistance("DistanceLeft")
+            print("Dist {}".format(dist))
+            time.sleep(0.2)
+            if dist != 0:
+                notZero += 1
+        self.assertNotEquals(0, notZero)
 
 
 if __name__ == '__main__':
